c6c415e6985a4db456722637cc013166da237b2a
durmus ali saymaz <dali@durmuss-MacBook-Pro.local>
2022-10-30 13:40:06 +0100

    dom yapisi eklendi

---
+++ 18-DOM-Event/2/script.js
@@ -0,0 +1,14 @@
+
+//document.getElementsByTagName()
+let dali;
+dali = document.getElementsByTagName('li');
+console.log(dali)
+dali = document.getElementById('task-list');
+console.log(dali)
+dali = dali.getElementsByTagName('a');
+console.log(dali)
+
+
+// document.querySelectorAll()
+dali = document.querySelectorAll('li');
+console.log(dali)
